4d5c5572665797a3da48175735f9118c393274fd
PrachiKhajuria21 <prachi.khajuria.23@gmail.com>
2023-05-22 17:09:07 +0530

    sorting searching with typescript

--- src/Pages/Table/TableData.tsx
+++ src/Pages/Table/TableData.tsx
@@ -1,83 +1,91 @@
 // import PageMeta from "./TableComponent";
 import { useEffect, useState } from "react";
+import { string } from "yup";
 import { InitialStateType } from "../../model";
 // import PageMeta from "./TableComponent";
 // import PageMeta from "./TableComponent";
 
-interface Props{
-  data:InitialStateType[];
+interface Props {
+  data: InitialStateType[];
 }
 
-const TableData = ({ data }:Props) => {
-  const [order, setOrder] = useState("asc");
+const TableData = ({ data }: Props) => {
+  const [order, setOrder] = useState<string>("asc");
   const [search, setSearch] = useState("");
 
-  const [tableData, setTableData] = useState<InitialStateType[]>();
+  const [tableData, setTableData] = useState<InitialStateType[]>(data);
 
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setTableData(data);
-    }
-  }, [data]);
+  // useEffect(() => {
+  //   if (data && data.length > 0) {
+  //     setTableData(data);
+  //   }
+  // }, [data]);
 
-  const records:number = 2;
-  const totalRecords:number = data.length;
+  const records: number = 5;
+  const totalRecords: number = data.length;
 
-  let pages:number = Math.ceil(totalRecords / records);
-  
-  const pageNumber = [...Array(pages+1).keys()].slice(1)
-  ;
+  let pages: number = Math.ceil(totalRecords / records);
 
+  const pageNumber = [...Array(pages + 1).keys()].slice(1);
   const [currentPage, setCurrentPage] = useState(1);
 
   const indexOfLast = records * currentPage;
   const indexOfFirst = indexOfLast - records;
 
-  const handlePagination = (number:number) => {
+  const handlePagination = (number: number) => {
     setCurrentPage(number);
   };
 
-  // const handleTableData = ({ sort = "", searchValue = search }) => {
-  //   let temp = [...data];
-  //   let tempOrder = order;
+  const handleTableData = ({
+    sort = "id",
+    searchValue = search,
+  }: {
+    sort?: keyof InitialStateType;
+    searchValue?: string;
+  }) => {
+    let temp = [...data];
+    let tempOrder = order;
 
-  //   if (sort) {
-  //     // Sorting
-  //     if (tempOrder === "asc") {
-  //       temp = temp.sort((a, b) => (a[sort] < b[sort] ? -1 : 1));
+    if (sort) {
+      // Sorting
+      if (tempOrder === "asc") {
+        temp = temp.sort((a: InitialStateType, b: InitialStateType) =>
+          a[sort] < b[sort] ? -1 : 1
+        );
 
-  //       tempOrder = "desc";
-  //     } else if (tempOrder === "desc") {
-  //       temp = temp.sort((a, b) => (a[sort] > b[sort] ? -1 : 1));
-  //       tempOrder = "";
-  //     } else {
-  //       temp = data;
-  //       tempOrder = "asc";
-  //     }
-  //     setOrder(tempOrder);
-  //   }
+        tempOrder = "desc";
+      } else if (tempOrder === "desc") {
+        temp = temp.sort((a: any, b: any) => (a[sort] > b[sort] ? -1 : 1));
+        tempOrder = "";
+      } else {
+        temp = data;
+        tempOrder = "asc";
+      }
+      setOrder(tempOrder);
+    }
 
-  //   // Searching
+    // Searching
 
-  //   temp = temp.filter((curData) => {
-  //     if (
-  //       curData.month.toLocaleLowerCase().includes(searchValue.trim()) ||
-  //       curData.date.includes(searchValue.trim()) ||
-  //       curData.transactionType.includes(searchValue.trim()) ||
-  //       curData.fromAccount.includes(searchValue.trim()) ||
-  //       curData.toAccount.includes(searchValue.trim()) ||
-  //       curData.notes.includes(searchValue.trim()) ||
-  //       curData.amount.toString().includes(searchValue.trim())
-  //     ) {
-  //       return curData;
-  //     } else {
-  //       return "";
-  //     }
-  //   });
+    temp = temp.filter((curData) => {
+      if (
+        curData.month.toLocaleLowerCase().includes(searchValue.trim()) ||
+        curData.date.includes(searchValue.trim()) ||
+        curData.transactionType.includes(searchValue.trim()) ||
+        curData.fromAccount.includes(searchValue.trim()) ||
+        curData.toAccount.includes(searchValue.trim()) ||
+        curData.notes.includes(searchValue.trim()) ||
+        curData.amount.toString().includes(searchValue.trim())
+      ) {
+        return curData;
+      } else {
+        return "";
+      }
+    });
 
-  //   setTableData(temp);
-  // };
+    setTableData(temp);
+  };
 
+  console.log("orderdata", tableData);
 
   const mystyle = {
     border: "2px solid green",
@@ -101,14 +109,13 @@ const TableData = ({ data }:Props) => {
   const tableHeaderTrans = {
     paddingLeft: "20px",
   };
-  
 
-  // useEffect(() => {
-  //   handleTableData({ searchValue: search });
-  // }, [search]);
+  useEffect(() => {
+    handleTableData({ searchValue: search });
+  }, [search]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("search::::",event.target.value)
+    console.log("search::::", event.target.value);
     setSearch(event.target.value);
   };
 
@@ -118,16 +125,15 @@ const TableData = ({ data }:Props) => {
     cursor: "pointer",
   };
 
-  // const getArrow = (order) => {
-
-  //   if (order === "asc") {
-  //     return "";
-  //   } else if (order === "desc") {
-  //     return "↑";
-  //   } else {
-  //     return "↓";
-  //   }
-  // };
+  const getArrow = (order:string) => {
+    if (order === "asc") {
+      return "";
+    } else if (order === "desc") {
+      return "↑";
+    } else {
+      return "↓";
+    }
+  };
 
   const tableHeader = {
     padding: "5px",
@@ -150,55 +156,55 @@ const TableData = ({ data }:Props) => {
     <div>
       <div>
         <label style={searchClass}>Search</label>
-        <input type="text" 
-        onChange={handleSearch}
-         />
+        <input type="text" onChange={handleSearch} />
       </div>
       <div className="dropDown"></div>
       <table style={mystyle}>
         <tbody>
           <tr>
-          <th style={headerType}>
-              <button style={tableHeader}>Date</button>
-            </th>
-            <th 
-            // onClick={() => handleTableData({ sort: "month" })}
+            <th>ID</th>
+            <th
+              style={headerType}
+              onClick={() => handleTableData({ sort: "date" })}
             >
-              <button style={tableHeader}> Month Year 
-              {/* {getArrow(order)} */}
+              <button style={tableHeader}>Date{getArrow(order)}</button>
+            </th>
+            <th onClick={() => handleTableData({ sort: "month" })}>
+              <button style={tableHeader}>
+                Month Year
+                {getArrow(order)}
               </button>
             </th>
             <th
-              // onClick={() => handleTableData({ sort: "transactionType" })}
+              onClick={() => handleTableData({ sort: "transactionType" })}
+              style={headerType}
+            >
+              <button style={tableHeader}>
+                Transaction Type {getArrow(order)}{" "}
+              </button>
+            </th>
+            <th
+              onClick={() => handleTableData({ sort: "fromAccount"})}
               style={headerType}
             >
               <button style={tableHeader}>
                 {" "}
-                Transaction Type 
-                {/* {getArrow(order)} */}
+                From Account
+                {getArrow(order)}
               </button>
             </th>
-            <th 
-            // onClick={() => handleTableData({ sort: "fromAccount" })}
-              style={headerType}>
+            <th onClick={() => handleTableData({ sort: "toAccount" })}>
               <button style={tableHeader}>
                 {" "}
-                From Account 
-                {/* {getArrow(order)} */}
+                To Account
+                {getArrow(order)}
               </button>
             </th>
-            <th 
-            // onClick={() => handleTableData({ sort: "toAccount" })}
-            >
-              <button style={tableHeader}> To Account
-               {/* {getArrow(order)} */}
-               </button>
-            </th>
-            <th 
-            // onClick={() => handleTableData({ sort: "amount" })}
-            >
-              <button style={tableHeader}> Amount 
-              {/* {getArrow(order)} */}
+            <th onClick={() => handleTableData({ sort: "amount"})}>
+              <button style={tableHeader}>
+                {" "}
+                Amount
+                {getArrow(order)}
               </button>
             </th>
             <th style={headerType}>
@@ -206,10 +212,12 @@ const TableData = ({ data }:Props) => {
             </th>
             <th
               style={headerType}
-              // onClick={() => handleTableData({ sort: "notes" })}
+              onClick={() => handleTableData({ sort: "notes" })}
             >
-              <button style={tableHeader}> Notes 
-              {/* {getArrow(order)} */}
+              <button style={tableHeader}>
+                {" "}
+                Notes
+                {getArrow(order)}
               </button>
             </th>
             <th>
@@ -222,39 +230,38 @@ const TableData = ({ data }:Props) => {
 
           {/* <PageMeta datar={tableData} /> */}
 
-        
-      {data.length > 0 &&
-        data.slice(indexOfFirst,indexOfLast).map((data, index) => (
-          <tr key={index}>
-            {/* <td>{datar.id}</td> */}
-            <td style={tableHeaderTrans}>{data.date}</td>
-            <td style={tableHeaderTrans}>{data.month}</td>
-            <td style={tableHeaderTrans}>{data.transactionType}</td>
-            <td style={tableHeaderTrans}>{data.fromAccount}</td>
-            <td style={tableHeaderTrans}>{data.toAccount}</td>
-            <td style={tableHeaderTrans}>{data.amount}</td>
-            <td style={tableHeaderTrans}>
-              <img src={data.receipt} alt="test" />
-            </td>
-            <td style={tableHeaderTrans}>{data.notes}</td>
-            <td>
-              <button style={edit} 
-              // onClick={() => handleEdit(datar.id)}
-              >
-                Edit
-              </button>
-            </td>
-            <td>
-              <button style={edit} 
-              // onClick={() => handleRemove(datar.id)}
-              >
-                Remove
-              </button>
-            </td>
-          </tr>
-        ))}
-    
-
+          {tableData.length > 0 &&
+            tableData.slice(indexOfFirst, indexOfLast).map((data, index) => (
+              <tr key={index}>
+                <td>{data.id}</td>
+                <td style={tableHeaderTrans}>{data.date}</td>
+                <td style={tableHeaderTrans}>{data.month}</td>
+                <td style={tableHeaderTrans}>{data.transactionType}</td>
+                <td style={tableHeaderTrans}>{data.fromAccount}</td>
+                <td style={tableHeaderTrans}>{data.toAccount}</td>
+                <td style={tableHeaderTrans}>{data.amount}</td>
+                <td style={tableHeaderTrans}>
+                  <img src={data.receipt} alt="test" />
+                </td>
+                <td style={tableHeaderTrans}>{data.notes}</td>
+                <td>
+                  <button
+                    style={edit}
+                    // onClick={() => handleEdit(data.id)}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    style={edit}
+                    // onClick={() => handleRemove(datar.id)}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
       <div style={pageStyle}>
@@ -263,7 +270,6 @@ const TableData = ({ data }:Props) => {
             <span
               style={pageBottom}
               key={number}
-
               onClick={() => handlePagination(number)}
             >
               {number}
@@ -272,6 +278,6 @@ const TableData = ({ data }:Props) => {
       </div>
     </div>
   );
-}
+};
 
 export default TableData;
